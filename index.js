bdd7c1337a7889caac2d3f3f971fbe582eaa6342
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2023-02-07 15:31:37 +0200

    Updated CORS policy

--- index.js
+++ index.js
@@ -9,15 +9,11 @@ const app = express();
 const PORT = 3000;
 // app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
-// app.use(
-//   cors({
-//     origin: [
-//       "https://agile-final.netlify.app/doctor-form",
-//       "https://agile-final.netlify.app",
-//       "https://agile-final.netlify.app/login, https://agile-final.netlify.app/home-page, http://127.0.0.1:5500/index.html, http://127.0.0.1:5500",
-//     ],
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://little-cafe.netlify.app"],
+  })
+);
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(router);
